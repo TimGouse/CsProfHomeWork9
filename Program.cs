4aee5838e60617a17e69efaeff1f472508bb4448
Тим Гусь <guskov.tima@rambler.ru>
2023-11-22 19:00:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CsProfHomeWork9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем и клонируем объекты каждого класса
+            var vehicle = new Vehicle("Generic", "Model1");
+            var clonedVehicle = vehicle.Clone();
+
+            var car = new Car("Generic", "Model2", 4, "Petrol");
+            var clonedCar = car.Clone();
+
+            var electricCar = new ElectricCar("Generic", "Model3", 4, "Electric", 500, 300);
+            var clonedElectricCar = electricCar.Clone();
+
+            // Вывод информации о каждом клонированном объекте
+            Console.WriteLine($"Cloned Vehicle: {clonedVehicle.Brand}, {clonedVehicle.Model}");
+            Console.WriteLine($"Cloned Car: {clonedCar.Brand}, {clonedCar.Model}, {clonedCar.NumberOfDoors}, {clonedCar.EngineType}");
+            Console.WriteLine($"Cloned ElectricCar: {clonedElectricCar.Brand}, {clonedElectricCar.Model}, {clonedElectricCar.NumberOfDoors}, {clonedElectricCar.EngineType}, {clonedElectricCar.BatteryCapacity}, {clonedElectricCar.RangePerCharge}");
+        }
+    }
+}
---
+++ Tracks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProfHomeWork9
+{
+    public class Vehicle : ICloneable, IMyCloneable<Vehicle>
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Vehicle(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        object ICloneable.Clone() => Clone();
+
+        public Vehicle Clone() => new Vehicle(Brand, Model);
+    }
+
+    // Класс Car, наследующийся от Vehicle
+    public class Car : Vehicle, IMyCloneable<Car>
+    {
+        public int NumberOfDoors { get; set; }
+        public string EngineType { get; set; }
+
+        public Car(string brand, string model, int numberOfDoors, string engineType)
+            : base(brand, model)
+        {
+            NumberOfDoors = numberOfDoors;
+            EngineType = engineType;
+        }
+
+        public new Car Clone() => new Car(Brand, Model, NumberOfDoors, EngineType);
+    }
+
+    // Класс ElectricCar, наследующийся от Car
+    public class ElectricCar : Car, IMyCloneable<ElectricCar>
+    {
+        public int BatteryCapacity { get; set; }
+        public int RangePerCharge { get; set; }
+
+        public ElectricCar(string brand, string model, int numberOfDoors, string engineType, int batteryCapacity, int rangePerCharge)
+            : base(brand, model, numberOfDoors, engineType)
+        {
+            BatteryCapacity = batteryCapacity;
+            RangePerCharge = rangePerCharge;
+        }
+
+        public new ElectricCar Clone() => new ElectricCar(Brand, Model, NumberOfDoors, EngineType, BatteryCapacity, RangePerCharge);
+    }
+}
